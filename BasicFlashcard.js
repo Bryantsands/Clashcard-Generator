cd1a7a520aed1ad24cc728646bbfcbd22ea82b8d
bryantsands <bryantsands@bryantsandstekiMacBook-Pro.local>
2016-12-14 11:08:43 -0800

    ready

--- BasicFlashcard.js
+++ BasicFlashcard.js
@@ -3,21 +3,21 @@ var fs = require("fs");
 
 
 // constructor function for basicflashcard
-function BasicFlashcard(front, back) {
-    this.front = front;
-    this.back = back;
+function BasicFlashcard(question, answer) {
+    this.question = question;
+    this.answer = answer;
 };
 
 // creates the printInfo method and applies it to all basicflashcard objects
 BasicFlashcard.prototype.printInfo = function(){
-        console.log("Question: " + this.front + "\nAnswer is: " + this.back)
+        console.log("Question: " + this.question + "\nAnswer is: " + this.answer)
     };
 
 // new basicFlashcard object is initialized to nextcard and is provided with the variables
 // necessary to create all of the properties
-var newCard = new BasicFlashcard("who is the president", "obama");
+// var newCard = new BasicFlashcard("who is the president", "obama");
 
-newCard.printInfo(); 
+// newCard.printInfo(); 
 
 
 

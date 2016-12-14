26db1839ba5589a443a08ab3775a175b4f7d9757
bryantsands <bryantsands@bryantsandstekiMacBook-Pro.local>
2016-12-14 08:40:59 -0800

    "initial commit"

--- BasicFlashcard.js
+++ BasicFlashcard.js
@@ -1,13 +1,25 @@
 var inquirer = require("inquirer");
 var fs = require("fs");
 
+
+// constructor function for basicflashcard
 function BasicFlashcard(front, back) {
     this.front = front;
     this.back = back;
-    
-    var NewCard = new BasicFlashcard();
-
-    console.log(NewCard);
 };
+
+// creates the printInfo method and applies it to all basicflashcard objects
+BasicFlashcard.prototype.printInfo = function(){
+        console.log("Question: " + this.front + "\nAnswer is: " + this.back)
+    };
+
+// new basicFlashcard object is initialized to nextcard and is provided with the variables
+// necessary to create all of the properties
+var newCard = new BasicFlashcard("who is the president", "obama");
+
+newCard.printInfo(); 
+
+
+
 
 module.exports = BasicFlashcard;
